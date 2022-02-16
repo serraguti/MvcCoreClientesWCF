211d669ecafa2d00bd4cb870c49134eacf401c04
serraguti <pacoserranox@gmail.com>
2022-02-16 14:06:48 +0100

    Add project files.

---
+++ MvcCoreClientesWCF/Controllers/CountriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreClientesWCF.Services;
+using ServicioCountries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreClientesWCF.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private ServiceCountries service;
+
+        public CountriesController(ServiceCountries service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            tCountryCodeAndName[] countries =
+                await this.service.GetCountries();
+            return View(countries);
+        }
+    }
+}
---
+++ MvcCoreClientesWCF/Services/ServiceCountries.cs
@@ -0,0 +1,32 @@
+﻿using ServicioCountries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreClientesWCF.Services
+{
+    public class ServiceCountries
+    {
+        //EL NOMBRE DEL OBJETO SIEMPRE SERA UN CLIENT
+        //SE LLAMARA COMO EL NOMBRE DEL SERVICIO QUE HEMOS
+        //VISTO EN LA ANTERIOR PANTALLA
+        CountryInfoServiceSoapTypeClient client;
+
+        public ServiceCountries()
+        {
+            this.client = new CountryInfoServiceSoapTypeClient
+(CountryInfoServiceSoapTypeClient.EndpointConfiguration.CountryInfoServiceSoap);
+        }
+
+        public async Task<tCountryCodeAndName[]> 
+            GetCountries()
+        {
+            ListOfCountryNamesByNameResponse response =
+            await this.client.ListOfCountryNamesByNameAsync();
+            tCountryCodeAndName[] objetos =
+            response.Body.ListOfCountryNamesByNameResult;
+            return objetos;
+        }
+    }
+}
